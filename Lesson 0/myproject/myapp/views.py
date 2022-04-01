187165b14825d686b1582af4b04a53e06cb8917b
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-04-01 15:51:38 +0300

    added database

--- Lesson 0/myproject/myapp/views.py
+++ Lesson 0/myproject/myapp/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+products = [
+    {'id': 1, 'name': 'watch', 'price': 100},
+    {'id': 2, 'name': 'hoodie', 'price': 40},
+]
+
 
 def home(request):
     # Python logic
 
     # Data
-    context = {}
+    context = {
+        "products": products
+    }
 
     # Which html you want to link
     return render(request, 'index.html', context)
 
 
 def watch(request):
-    return HttpResponse('watch')
+    context = {
+        "products": products
+    }
+    return render(request, 'index.html', context)
