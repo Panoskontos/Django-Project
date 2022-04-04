cc125eaad25988f335b3c35b10f2075dd118497d
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-04-04 22:12:39 +0300

    added mysql

--- Lesson 0/myproject/myapp/urls.py
+++ Lesson 0/myproject/myapp/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('example/',example, name='example'),
     path('product/<str:name>',product, name='product'),
     path('create_new_product',create_new_product, name='create_new_product'),
+    path('update_product/<str:pk>',update_product, name='update_product'),
+    path('delete_product/<str:pk>',delete_product, name='delete_product'),
+    path('success/<str:message>',success, name='success'),
+    
+    
     path('category/<str:category>/',product_by_category, name="product_by_category")
 ]
--- Lesson 0/myproject/myapp/views.py
+++ Lesson 0/myproject/myapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 from .models import *
 from .forms import ProductForm
@@ -40,7 +40,7 @@ def example(request):
     # για ολα βαζεις all()
     products = Product.objects.all()
 #  για 1 βαζεις get()
-    one_product = Product.objects.get(name="Laptop")
+    # one_product = Product.objects.get(name="Laptop")
 
     # για συγκεκριμενη ομαδα βαζεις .filter()
     # lte = lower than equal
@@ -51,8 +51,8 @@ def example(request):
 
     context = {
         'products':products,
-        'one_product':one_product,
-        'cheap_products':cheap_products,
+        # 'one_product':one_product,
+        # 'cheap_products':cheap_products,
     }
     return render(request, 'myapp/example.html',context)
 
@@ -92,11 +92,44 @@ def create_new_product(request):
         
         form = ProductForm(request.POST)
         if form.is_valid():
+            message = 'Item was created with success'
             form.save()
+            return redirect('success',message)
     context = {
         'form':form,
     }
     return render(request, 'myapp/form.html', context)
+
+
+def update_product(request,pk):
+    product = Product.objects.get(id=pk)
+    form = ProductForm(instance=product)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            message = 'Item was updated with success'
+            form.save()
+            return redirect('success',message)
+    context = {
+        'form':form,
+    }
+    return render(request, 'myapp/form.html', context)
+
+
+def delete_product(request, pk):
+    # logic
+    product = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        product.delete()
+        
+
+
+    # Versatility
+    object = 'Laptop'
+    context = {
+        'object':object,
+    }
+    return render(request, 'myapp/delete.html', context)
 
 
 def product_by_category(request,category):
@@ -107,3 +140,8 @@ def product_by_category(request,category):
         'category':category,
     }
     return render(request, 'myapp/product_by_category.html', context)
+
+# Versatile
+def success(request, message):
+    return HttpResponse(str(message))
+      
